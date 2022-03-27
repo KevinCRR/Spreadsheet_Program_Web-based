1228025a428284f0a43c8279bd755aaa3a997bb7
KevinCRR <47986882+KevinCRR@users.noreply.github.com>
2022-03-27 00:08:33 -0400

    Added more chart stuff

--- scripts/spreadsheet.js
+++ scripts/spreadsheet.js
@@ -2,19 +2,81 @@ var textCache;
 var activebutton = null;
 var colData = [];
 var jsonData;
+var averages = [];
 
 //****************** GRAPH STUFF *****************/
-var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+function getGrade(mark) {
+  if (mark < 50.0) {
+    return "F";
+  } else if (mark < 60.0) {
+    return "D";
+  } else if (mark < 70.0) {
+    return "C";
+  } else if (mark < 80.0) {
+    return "B";
+  } else {
+    return "A";
+  }
+}
 
-var svg = d3
-  .select("#data")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function getTableData(data) {
+  const width = 800;
+  const height = 500;
+  const margin = 50;
+  const chartWidth = width - 2 * margin; // 700
+  const chartHeight = height - 2 * margin; // 400
+
+  const colorScale = d3
+    .scaleLinear()
+    .domain([734, 1859])
+    .range(["yellow", "green"]);
+
+  const xScale = d3
+    .scaleBand()
+    .domain(Object.entries(data).map(([k, v]) => k))
+    .range([0, chartWidth])
+    .padding(0.5);
+
+  const yScale = d3.scaleLinear().domain([0, 0.5]).range([chartHeight, 0]);
+
+  let svg = d3
+    .select(".main-content-wrapper")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", height - 10)
+    .attr("text-anchor", "middle")
+    .text("Grade");
+
+  let graph = svg
+    .append("g")
+    .attr("transform", `translate(${margin},${margin})`);
+
+  graph
+    .append("g")
+    .attr("transform", `translate(0,${chartHeight})`)
+    .call(d3.axisBottom(xScale));
+
+  graph.append("g").call(d3.axisLeft(yScale));
+
+  let rectangles = graph
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (data) => xScale());
+
+  var g = svg
+    .append("g")
+    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+  d3.json(data, function (data) {
+    console.log(data);
+  });
+}
 
 // finds all td's with active class and removes the class.
 function deselectAll() {
@@ -32,64 +94,20 @@ function selectRow(rowIndex) {
     });
 }
 
-function getTableData() {
-  // get the data
-  d3.json(jsonData, function (data) {
-    // X axis: scale and draw:
-    var x = d3
-      .scaleLinear()
-      .domain([0, 1000]) // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-    svg
-      .append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    // set the parameters for the histogram
-    var histogram = d3
-      .histogram()
-      .value(function (d) {
-        return d.price;
-      }) // I need to give the vector of value
-      .domain(x.domain()) // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
-
-    // And apply this function to data to get the bins
-    var bins = histogram(data);
-
-    // Y axis: scale and draw:
-    var y = d3.scaleLinear().range([height, 0]);
-    y.domain([
-      0,
-      d3.max(bins, function (d) {
-        return d.length;
-      }),
-    ]); // d3.hist has to be called before the Y axis obviously
-    svg.append("g").call(d3.axisLeft(y));
-
-    // append the bar rectangles to the svg element
-    svg
-      .selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-      .attr("x", 1)
-      .attr("transform", function (d) {
-        return "translate(" + x(d.x0) + "," + y(d.length) + ")";
-      })
-      .attr("width", function (d) {
-        return x(d.x1) - x(d.x0) - 1;
-      })
-      .attr("height", function (d) {
-        return height - y(d.length);
-      })
-      .style("fill", "#69b3a2");
-  });
-}
-
 //selects all rows where it's index is equals to the index of the column clicked.
 function selectColumn(colIndex) {
   var i = 0;
+  var value;
+  var averagesJson = null;
+  averages = [
+    { letter: "A", count: 0 },
+    { letter: "B", count: 0 },
+    { letter: "C", count: 0 },
+    { letter: "D", count: 0 },
+    { letter: "F", count: 0 },
+  ];
+  colData = [];
+  jsonData = null;
   deselectAll();
   $("#spreadsheet")
     .find("tr")
@@ -99,24 +117,32 @@ function selectColumn(colIndex) {
         .eq(colIndex - 1)
         .addClass("active");
 
-      colData[i] = $(col)
+      value = $(col)
         .find("td")
         .eq(colIndex - 1)
         .text();
 
-      if (!colData[i]) {
+      if (!value) {
         i--;
-        colData.pop();
       } else {
-        console.log(colData[i]);
+        colData.push({
+          grade: value,
+          letter: getGrade(value),
+        });
+
+        for (const [key, value] of dict) {
+          console.log(key + ": " + value);
+        }
+        averages[getGrade(value)] = averages[getGrade(value)] + 1;
       }
       i++;
     });
 
-  console.log(colData);
-  jsonData = JSON.stringify(colData);
+  jsonData = Object.assign({}, colData);
+  averagesJson = Object.assign({}, averages);
+  console.log(averagesJson);
   console.log(jsonData);
-  getTableData();
+  // getTableData(jsonData);
 }
 
 // waits for the document to be fully loaded.
@@ -146,15 +172,9 @@ $("td").click(function (e) {
   $("input").keypress(function (e) {
     if (e.which == "13" && activebutton) {
       activebutton = null;
-      console.log("you pressed enter");
       $(this).parent().removeClass("active");
       $(this).parent().text($(this).val());
     }
-    // } else {
-    //   console.log("you pressed something else");
-    //   $(this).parent().removeClass("active");
-    //   $(this).parent().text(textCache);
-    // }
   });
 });
 
@@ -163,20 +183,3 @@ $("td").on("blur", "input", function () {
   $(this).parent().text(textCache);
   activebutton = null;
 });
-
-// $(this).parent().text(textCache);
-
-// $("input").keydown(function (e) {
-//   if ((e.keyCode || e.which) == 13) {
-//     console.log("Enter key pressed");
-//     $(this).parent().removeClass("active");
-//     $(this).text(textCache);
-//   }
-// });
-// $("td").keydown(function (e) {
-//   if ((e.keyCode || e.which) == 13) {
-//     console.log("Enter key pressed");
-//     $(this).removeClass("active");
-//     $(this).text(textCache);
-//   }
-// });
